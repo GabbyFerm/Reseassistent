9ed5044606401e40d8d2e129492af2069fd9aa01
Gabby <gabbzf@gmail.com>
2024-12-09 10:35:21 +0100

    created json db

---
+++ Reseassistent/Classes/TravelDB.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Reseassistent.Classes
+{
+    public class TravelDB
+    {
+        public List<Cities> Cities { get; set; } = new List<Cities>();
+
+        private readonly string _filePath;
+
+        public TravelDB(string filePath)
+        {
+            _filePath = filePath;
+
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            if (File.Exists(_filePath))
+            {
+                string json = File.ReadAllText(_filePath);
+                Cities = JsonSerializer.Deserialize<List<Cities>>(json) ?? new List<Cities>();
+            }
+        }
+
+        public void SaveData()
+        {
+            string json = JsonSerializer.Serialize(Cities, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
