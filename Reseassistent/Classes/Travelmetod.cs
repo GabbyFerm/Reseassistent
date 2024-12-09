31259f27f5c3314567ad757c7bc7d797477c90cd
ITHanan <155847905+ITHanan@users.noreply.github.com>
2024-12-09 11:24:28 +0100

    The atribute to Travelmethod

--- Reseassistent/Classes/Travelmetod.cs
+++ Reseassistent/Classes/Travelmetod.cs
@@ -2,5 +2,16 @@
 {
     public class Travelmetod
     {
+        public string Car { get; set; }
+
+        public string Train { get; set; }
+
+        public string Plane { get; set; }
+
+        public string Boat { get; set; }
+
+        public string Buss { get; set; }
+
+
     }
 }
